ffe52eefbc6cd4f88af35ca54a1239ecbfd8369f
nashiblmb <126723708+nashiblmb@users.noreply.github.com>
2023-03-02 05:32:12 +0000

    Create banker_roulette.py

---
+++ Beginner/practice_exercises/banker_roulette.py
@@ -0,0 +1,9 @@
+#Practice exercise creating a banker roulette
+import random
+
+names_string = input("Please enter names of participants with comma seperating the names: ")
+names_list = names_string.split(", ")
+
+max_index = random.randint(0, (len(names_list)))
+
+print(f"{names_list[max_index]} is going to buy the meal today!")
